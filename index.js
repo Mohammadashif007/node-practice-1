3953d99c758f839a0fb22bd7b557bdd07365c477
Mohammad Ashif <Mohammad Ashif>
2024-05-17 20:38:47 +0600

    node module

---
+++ index.js
@@ -0,0 +1,18 @@
+// const {add, a} = require('./local-1');
+// const {add:add2} = require('./local-2');
+
+
+// console.log(add2(3, 4, 8));
+
+const path = require("path");
+
+// console.log(path.extname("H:/Next-level/node/tempCodeRunnerFile.js"));
+// console.log(path.delimiter)
+// console.log(path.delimiter("H:/Next-level/node/tempCodeRunnerFile.js"));
+// console.log(path.format({
+//     root: "/H",
+//     dir: "/Next-level/node/",
+//     dir: "tempCodeRunnerFile.js",
+// }));
+
+console.log(path.join("H:/Next-level/node/tempCodeRunnerFile.js", "new.js"));
---
+++ local-2.js
@@ -0,0 +1,7 @@
+const add = (param1, param2, p3) => param1 + param2 + p3;
+
+const a = 10;
+
+module.exports = {
+    add, a
+}
